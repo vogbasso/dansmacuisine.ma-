17da9f6748fa4f7371bde19814272af3e23c805e
vogbasso <vogelpaolo@gmail.com>
2017-06-23 15:46:39 +0000

    modification du fichier wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@
 define('DB_NAME', 'dansmacuisine_live');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'mobidealz');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', 'f2ttZ42a');
